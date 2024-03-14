dde6924b36c839e610d9d2db49b7c3b645aecf7a
Alberto Fernandez Gonzalez <alberto.fernandez.gonzalez@ing.com>
2024-03-14 07:22:51 +0100

    Add list of docs

--- api/feature.js
+++ api/feature.js
@@ -11,14 +11,23 @@ app.http('feature', {
       await db.connect(process.env.MONGODB_URI);
       const connection = db.getConnection();
       const packages = connection.collection('packages');
-      const resource = await packages.findOne({});
-      // const resource = [];
-      // for await (const doc of cursor) {
-      //   resource.push(doc);
-      // }
+      const cursor = await packages.find({});
+      const resource = {};
+      for await (const doc of cursor) {
+        if(!resource[doc.namespace]) {
+          resource[doc.namespace] = [];
+        }
+
+        if (!resource[doc.namespace].includes(doc.locale)){
+          resource[doc.namespace].push(doc.locale);
+        }
+      }
 
       return {
-        body: JSON.stringify(resource)
+        body: JSON.stringify(Object.keys(resource).map(namespace => ({
+          namespace,
+          locales: resource[namespace],
+        })))
       };
     } catch (e) {
       console.log(e)
