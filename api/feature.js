a79ee8819feca40ccc08ba6a60e9ffd3d4d2376f
Yener Unver <yenerunver@hotmail.com>
2024-03-14 02:11:04 +0100

    extend feature endpoint

--- api/feature.js
+++ api/feature.js
@@ -4,6 +4,40 @@ app.http('feature', {
   methods: ['GET'],
   authLevel: 'anonymous',
   handler: async function (request, context) {
+    const locale = request.query.get('locale') || '';
+    const id = request.query.get('id') || '';
+
+    if (locale && id) {
+      return {
+        body: JSON.stringify(
+          {
+            "namespace": "ing-feat-feet",
+            "locale": "nl-NL",
+            "library": {
+              sample: 'json'
+            }
+          }
+        )};
+    }
+
+    if (id) {
+      return {
+        body: JSON.stringify(
+          {
+            "namespace": "ing-feat-feet",
+            "locales": ["en-GB", "nl-NL"],
+            "libraries": {
+              "en-GB": {
+                sample: 'json'
+              },
+              "nl-NL": {
+                sample: 'json'
+              }
+            }
+          }
+        )};
+    }
+
     return {
         body: JSON.stringify(
           [
