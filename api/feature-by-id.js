4dd31945a6c9088689caef6ece001db7f9d13b00
Yener Unver <yenerunver@hotmail.com>
2024-03-14 07:36:14 +0100

    fix feature-by-id endpoint

--- api/feature-by-id.js
+++ api/feature-by-id.js
@@ -1,8 +1,5 @@
 const { app } = require('@azure/functions');
-const db = require('./db.js')
-
-
-
+const db = require('./db.js');
 
 app.http('feature-by-id', {
   methods: ['GET'],
@@ -18,29 +15,28 @@ app.http('feature-by-id', {
     await db.connect(process.env.MONGODB_URI);
     const connection = db.getConnection();
     const packages = connection.collection('packages');
-    const cursor = await packages.find({});
-    const resource = await packages.findOne({});
-    // const resource = [];
-    // for await (const doc of cursor) {
-    //   resource.push(doc);
-    // }
+    const cursor = await packages.find({
+      namespace: request.params.id
+    });
 
-
-    if (!resource) {
+    if (cursor.length === 0) {
       return {
         status: 404,
         body: 'Resource not found',
       };
     }
 
+    const libraries = {};
+    for await (const doc of cursor) {
+      libraries[doc.locale] = doc.package;
+    }
+
     return {
       body: JSON.stringify(
         {
-          "namespace": id,
-          "locales": [],
-          "libraries": resource,
-          "request": request,
-          "context": context
+          "namespace": request.params.id,
+          "locales": Object.keys(libraries),
+          "libraries": libraries
         }
       )
     };
